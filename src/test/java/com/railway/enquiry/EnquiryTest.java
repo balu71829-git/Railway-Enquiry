629373171292b35d4d7bb81c8e8642392c4324d5
balu71829-git <balu71829@gmail.com>
2025-12-02 10:35:37 +0530

    Initial commit of Railway Enquiry project

---
+++ src/test/java/com/railway/enquiry/EnquiryTest.java
@@ -0,0 +1,12 @@
+package com.railway.enquiry;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EnquiryTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);  // simple test just to generate report
+    }
+}
